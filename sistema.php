41d39c1ff6e99fc8a1a9f287795612a19fdd1e83
Andressa Rezende <andressa.122rezende@gmail.com>
2024-04-15 17:11:44 -0300

    sistema.php

--- sistema.php
+++ sistema.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include_once('config.php');
+    include_once('zconexao.php');
     // print_r($_SESSION);
     if((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true))
     {
@@ -20,120 +20,330 @@
     }
     $result = $conexao->query($sql);
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <title>SISTEMA | GN</title>
-    <style>
-        body{
-            background: linear-gradient(to right, rgb(20, 147, 220), rgb(17, 54, 71));
-            color: white;
-            text-align: center;
-        }
-        .table-bg{
-            background: rgba(0, 0, 0, 0.3);
-            border-radius: 15px 15px 0 0;
-        }
 
-        .box-search{
-            display: flex;
-            justify-content: center;
-            gap: .1%;
-        }
-    </style>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Central de pedidos</title>
+	<link rel="stylesheet" href="">
+
+	<style>
+
+		.todo1{
+			background-color: #7b4ff4;
+			width: 1500px;
+			height: 1850px;
+			margin-left: 300px;
+			margin-top: 150px;
+		
+			
+		}
+
+		.todo{
+			background-color: #1006a3;
+			width: 1020px;
+			height: 1280px;
+			margin-top:-120px;
+			margin-left: 200px;
+			border-radius: 20px;
+			border: solid 30px #1809cc;
+			box-shadow: 0 0 100px 0 #76C0E6; 
+		}
+
+		.d1{
+			background-color:  #7555da;
+			border-radius: 10px;
+			width: 200px;
+			height: 580px;
+			justify-items: center;
+			padding: 20px;
+			margin: 20px;
+
+		}
+
+		.d2{
+			background-color: #764af4;
+			border-radius: 10px;
+			width: 200px;
+			height: 580px;
+			left: 250px;
+			top: -640px;
+			position: relative;
+			justify-items: center;
+			padding: 20px;
+			margin: 20px;
+		}
+
+		.d3{
+			background-color: #c68bfd;
+			border-radius: 10px;
+			width: 200px;
+			height: 580px;
+			left: 500px;
+			top: -1280px;
+			position: relative;
+			justify-items: center;
+			padding: 20px;
+			margin: 20px;
+		}
+
+		.d4{
+			background-color: #8255ff;
+			border-radius: 10px;
+			width: 200px;
+			height: 580px;
+			left: 750px;
+			top: -1920px;
+			position: relative;
+			justify-items: center;
+			padding: 20px;
+			margin: 20px;
+		}
+
+		.d5{
+			
+			background-color: #7354de;
+			border-radius: 10px;
+			width: 200px;
+			height: 580px;
+			top: -1910px;
+			position: relative;
+			justify-items: center;
+			padding: 20px;
+			margin: 20px;
+		}
+
+		.d6{
+			background-color:#7349f3;
+			border-radius: 10px;
+			width: 200px;
+			height: 580px;
+			left: 500px;
+			top: -2550px;
+			position: relative;
+			justify-items: center;
+			padding: 20px;
+			margin: 20px;
+		}
+
+		.d7{
+			background-color: #3b1de0;
+			border-radius: 10px;
+			width: 200px;
+			height: 580px;
+			left: 750px;
+			top: -3190px;
+			position: relative;
+			justify-items: center;
+			padding: 20px;
+			margin: 20px;
+		}
+
+		.d8{
+			
+			background-color: #ae79ff;
+			border-radius: 10px;
+			width: 200px;
+			height: 580px;
+			top: -3830px;
+			left: 250px;
+			position: relative;
+			justify-items: center;
+			padding: 20px;
+			margin: 20px;
+		}
+
+		.h1{
+
+			background-color: #000079;
+			top: -1400px;
+			width: 1500px;
+			height: 220px;
+			color: white;
+			padding-top: 100px;
+			text-align: center;
+			font-family: Lucida Console;
+			font-size: 20px;
+		}
+
+		input{
+			border-radius: 10px;
+		}
+
+		label{
+			font-family: verdana;
+			font-weight: bold;
+		}
+		h2{
+			font-family: verdana;
+		}
+
+		.texto{
+			
+			top: -1200px;
+			width: 1500px;
+			height: 220px;
+			color: white;
+			text-align: center;
+			font-family: Lucida Console;
+			font-size: 20px;
+			margin-top: 70px;
+		}
+		
+	</style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="#">SISTEMA</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-        </div>
-        <div class="d-flex">
-            <a href="sair.php" class="btn btn-danger me-5">Sair</a>
-        </div>
-    </nav>
-    <br>
-    <?php
-        echo "<h1>Bem vindo <u>$logado</u></h1>";
+
+<div class="todo1">
+
+	<div class="h1">
+		<h1>Meus pedidos</h1>
+	<?php
+        echo "<h1> <u>$logado</u></h1>";
     ?>
-    <br>
-    <div class="box-search">
-        <input type="search" class="form-control w-25" placeholder="Pesquisar" id="pesquisar">
-        <button onclick="searchData()" class="btn btn-primary">
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-            </svg>
-        </button>
-    </div>
-    <div class="m-5">
-        <table class="table text-white table-bg">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nome</th>
-                    <th scope="col">Senha</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Telefone</th>
-                    <th scope="col">Sexo</th>
-                    <th scope="col">Data de Nascimento</th>
-                    <th scope="col">Cidade</th>
-                    <th scope="col">Estado</th>
-                    <th scope="col">Endereço</th>
-                    <th scope="col">...</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    while($user_data = mysqli_fetch_assoc($result)) {
-                        echo "<tr>";
-                        echo "<td>".$user_data['id']."</td>";
-                        echo "<td>".$user_data['nome']."</td>";
-                        echo "<td>".$user_data['senha']."</td>";
-                        echo "<td>".$user_data['email']."</td>";
-                        echo "<td>".$user_data['telefone']."</td>";
-                        echo "<td>".$user_data['sexo']."</td>";
-                        echo "<td>".$user_data['data_nasc']."</td>";
-                        echo "<td>".$user_data['cidade']."</td>";
-                        echo "<td>".$user_data['estado']."</td>";
-                        echo "<td>".$user_data['endereco']."</td>";
-                        echo "<td>
-                        <a class='btn btn-sm btn-primary' href='edit.php?id=$user_data[id]' title='Editar'>
-                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-pencil' viewBox='0 0 16 16'>
-                                <path d='M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z'/>
-                            </svg>
-                            </a> 
-                            <a class='btn btn-sm btn-danger' href='delete.php?id=$user_data[id]' title='Deletar'>
-                                <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-trash-fill' viewBox='0 0 16 16'>
-                                    <path d='M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z'/>
-                                </svg>
-                            </a>
-                            </td>";
-                        echo "</tr>";
-                    }
-                    ?>
-            </tbody>
-        </table>
-    </div>
+</div>
+
+<div class="texto">
+		<p> Reúna todos os seus <b>PEDIDOS</b> em um só lugar, sem a necessidade de ficar navegando de site em site. Facilite sua vida de compras!</p>
+</div>
+
+<div class="todo">
+		
+		<div class="d1"> 
+
+			<h2>Pedido 1</h2>
+
+			<label> Empresa: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Cidade-Estado: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Itens: 
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Valor <br> <br> <input type="text" name="pedido1" value=""> </label>
+		</div>
+
+		<div class="d2"> 
+
+			<h2>Pedido 2</h2>
+
+			<label> Empresa: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Cidade-Estado: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Itens: 
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Valor <br> <br> <input type="text" name="pedido1" value=""> </label>
+		</div>
+
+		<div class="d3"> 
+
+			<h2>Pedido 3</h2>
+
+			<label> Empresa: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Cidade-Estado: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Itens: 
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Valor <br> <br> <input type="text" name="pedido1" value=""> </label>
+		</div>
+
+		<div class="d4"> 
+
+			<h2>Pedido 4</h2>
+
+			<label> Empresa: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Cidade-Estado: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Itens: 
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Valor <br> <br> <input type="text" name="pedido1" value=""> </label>
+		</div>
+
+		<div class="d5"> 
+
+			<h2>Pedido 5</h2>
+
+			<label> Empresa: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Cidade-Estado: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Itens: 
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Valor <br> <br> <input type="text" name="pedido1" value=""> </label>
+		</div>
+
+		<div class="d6"> 
+
+			<h2>Pedido 7</h2>
+
+			<label> Empresa: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Cidade-Estado: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Itens: 
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Valor <br> <br> <input type="text" name="pedido1" value=""> </label>
+		</div>
+
+		<div class="d7"> 
+
+			<h2>Pedido 8</h2>
+
+			<label> Empresa: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Cidade-Estado: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Itens: 
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Valor <br> <br> <input type="text" name="pedido1" value=""> </label>
+		</div>
+
+		<div class="d8"> 
+
+			<h2>Pedido 6</h2>
+
+			<label> Empresa: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Cidade-Estado: <br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Itens: 
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value="">
+			<br> <br> <input type="text" name="pedido1" value=""> </label>
+			<label> <br> <br> Valor <br> <br> <input type="text" name="pedido1" value=""> </label>
+		</div>
+
+
+		
+</div>
+
+</div>
+	
 </body>
-<script>
-    var search = document.getElementById('pesquisar');
-
-    search.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") 
-        {
-            searchData();
-        }
-    });
-
-    function searchData()
-    {
-        window.location = 'sistema.php?search='+search.value;
-    }
-</script>
-</html>
+</html>             
